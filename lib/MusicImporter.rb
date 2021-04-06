6019ea82584d4777912bd5f7b645fccf47579236
Lrd134 <larryc3200@gmail.com>
2021-04-06 18:43:51 -0400

    commit tree 4276d2559e5448900f24add4f53aa97a46039ab6

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -2,18 +2,22 @@
 class MusicImporter
     attr_accessor :path, :files
     
-    def initialize(filepath = "./db/mp3s")
+    def initialize(filepath)
         @path = filepath
     end
     def files
+        
+
         @files = Dir["#{@path}/*"]
         @files.each do | obj |
             obj.slice! "#{@path}/"
-        end  
+        end
+
         @files
     end
     def import
         
+        files
         @files.each do | obj |
             Song.create_from_filename(obj)
         end
